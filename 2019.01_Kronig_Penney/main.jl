d86a1cde1ee6bef126de6d3bebdaec986941b8b0
Michael F. Herbst <info@michael-herbst.com>
2019-01-23 17:56:03 +0100

    Get KP code to work

--- 2019.01_Kronig_Penney/main.jl
+++ 2019.01_Kronig_Penney/main.jl
@@ -1,4 +1,5 @@
 using PyPlot
+using LinearAlgebra
 
 # We solve the Kronig-Penney model -Δ + V with
 # V = Z \sum δ_{an}
@@ -23,31 +24,32 @@ const Nb = length(qrange)
 @assert length(xrange) == Nb
 
 function build_ham()
-    A = zeros(Complex128,Nb,Nb)
-    for i=1:Nb,j=1:Nb
+    A = zeros(ComplexF64,Nb,Nb)
+    for i=1:Nb, j=1:Nb
         # A[i,j] = <qi|H|qj>
-        qi,qj = qrange[i],qrange[j]
+        qi, qj = qrange[i], qrange[j]
 
         # Kinetic
         if i == j
-            A[i,i] = abs2(qi)
+            A[i, i] = abs2(qi)
         end
 
         # Periodic Kronig-Penney potential
         for l=0:L-1
-            A[i,j] += Z*exp(im*(qj-qi)*a*l)/(a*L)
+            A[i, j] += Z*exp(im * (qj-qi) * a * l) / (a * L)
         end
 
         # Defect
         ldef = div(L,2)
-        A[i,j] += Zdef*exp(im*(qj-qi)*a*ldef)/(a*L)
+        A[i,j] += Zdef*exp(im * (qj-qi) * a * ldef) / (a * L)
     end
     return Hermitian(A)
 end
 
-# explicit fourier->real map (slow Fourier transform). Can be implemented more efficiently with an FFT
+# explicit fourier->real map (slow Fourier transform).
+# Can be implemented more efficiently with an FFT
 function build_four_to_real()
-    Q = zeros(Complex128,Nb,Nb)
+    Q = zeros(ComplexF64, Nb, Nb)
     for ix = 1:Nb
         for iq = 1:Nb
             Q[ix,iq] = exp(im*qrange[iq]*xrange[ix])/sqrt(a*L)
@@ -56,32 +58,38 @@ function build_four_to_real()
     Q
 end
 
-A = build_ham()
+A = build_ham()  # in q space
 Q = build_four_to_real()
-@assert diagm(diag(Q'Q)) ≈ (Q'Q)[1,1]*eye(Nb) #ensure unitarity up to constant factor
+@assert Diagonal(diag(Q'Q)) ≈ (Q'Q)[1,1]*I #ensure unitarity up to constant factor
 F2R = Q
 R2F = inv(Q)
 
-E,V = eig(A)
+E, V = eigen(A)
 # @assert (E[L+1] - E[L]) >  (E[L] - E[1]) #ensure good separation of bands
-# density matrix
-Pq = V[:,1:L]*V[:,1:L]' # in q space
-Px = F2R*Pq*R2F # in x space
+
+
+# Build density matrices and density
+Pq = V[:,1:L] * V[:,1:L]' # in q space
+Px = F2R * Pq * R2F       # in x space
 ρ = diag(Px)
 
 close(:all)
 
 # Plot the spectrum = collapsed band structure
 figure()
-plot(E,"x") # 
+plot(E,"x")
+title("Spectrum")
 
 # Plot the Bloch waves
 figure()
 for iplot=1:5
-    plot(real(F2R*V[:,iplot]))
+    plot(xrange, real(F2R*V[:,iplot]), label="Eigenvector $iplot")
 end
+legend()
+title("Bloch waves")
 
 # Plot the density
 figure()
-plot(xrange,ρ)
+plot(xrange, ρ)
+title("Density")
 # plot(xrange,(Px[div(end,2),:])) #locality of the density matrix
